a85008996d335650ec2ed4d6cbd01d7ffc2497d4
echizenyayota <virtualmisso30@gmail.com>
2018-01-01 19:01:26 +0900

    トップページの読み込み

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+// Sever Side Set Up
+// reference: https://developer.paypal.com/docs/accept-payments/express-checkout/ec-braintree-sdk/server-side/node/
 var express = require('express'),
-    // express.Router クラス　http://expressjs.com/ja/guide/routing.html
+    // router class　http://expressjs.com/ja/guide/routing.html
     router = express.Router(),
     braintree = require('braintree');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  // Construct your gateway object
+  var gateway = braintree.connect({
+    accessToken: useYourAccessToken
+  });
+
+  // Generate a client token
+  gateway.clientToken.generate({}, function (err, response) {
+    res.render('index', { clientToken: response.clientToken });
+  });
+
+}
